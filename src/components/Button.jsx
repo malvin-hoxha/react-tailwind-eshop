69bdfd33d649680bde503508d49567ad53e4f914
Malvin <malvinholearning@gmail.com>
2025-03-09 18:11:34 +0200

    Completing the project

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,31 @@
-const Button = ({label, iconUrl}) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7
-    py-4 border  font-montserrat text-lg leading-none bg-coral-red rounded-full text-white">
-        {label}
-        <img src={iconUrl} alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5" />
-    </button>
-  )
-}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
 
-export default Button
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
